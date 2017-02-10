7eae83233562319008e59f6ba9dd44bd8b01a784
Quentin RIBIERRE <quentin@prolificinteractive.com>
2017-02-10 00:28:52 -0500

    Created bot

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Charlie' });
 });
+
+const botBuilder = require('botbuilder')
+
+// Connection to Microsoft Bot Framework
+const botConnector = new botBuilder.ChatConnector({
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD
+})
+
+const bot = new botBuilder.UniversalBot(botConnector)
+
+// Event when Message received
+bot.dialog('/', (session) => {
+  console.log(session.message.text)
+})
+
+router.post('/', botConnector.listen)
 
 module.exports = router;

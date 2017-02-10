2f2ab4df3402302ee9868388dd0c407e399b6f99
Quentin RIBIERRE <quentin@prolificinteractive.com>
2017-02-10 01:31:30 -0500

    Return a bot response!

--- routes/bot_router.js
+++ routes/bot_router.js
@@ -6,21 +6,30 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Charlie' });
 });
 
-const botBuilder = require('botbuilder')
-
 // Connection to Microsoft Bot Framework
+const botBuilder = require('botbuilder')
 const botConnector = new botBuilder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
   appPassword: process.env.MICROSOFT_APP_PASSWORD
 })
-
 const bot = new botBuilder.UniversalBot(botConnector)
+
+// Recast
+const recast = require('recastai')
+const recastClient = new recast.Client(process.env.RECAST_AI)
 
 // Event when Message received
 bot.dialog('/', (session) => {
-  console.log(session.message.text)
+    if(session.message.text.toLowerCase().indexOf("charlie") > -1) {
+      recastClient.textRequest(session.message.text)
+      // .then(res => console.log(res))
+      .then(res => session.send("I'll be able to talk to you soon, promise!"),
+                   session.send("I'm still learning 🤓"))
+      .catch(() => session.send('I need some sleep right now... Talk to me later!'));
+    }
 })
 
+/* POST Bot interactions. */
 router.post('/', botConnector.listen())
 
 module.exports = router;
